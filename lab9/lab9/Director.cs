d6b1556a8fc4facf25fa528a0ce8e070740d452c
Kirill Pochta <70893878+KirillPochta@users.noreply.github.com>
2020-12-10 12:41:33 +0300

    Add files via upload

---
+++ lab9/lab9/Director.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab9
+{
+    class Director
+    {
+        public delegate void DirectorHandler(string message);
+        public event DirectorHandler Promote;
+        public event DirectorHandler Punishment;
+
+        public string name;
+        public int money;
+        public int position;
+
+        public Director(string name, int money, int position)
+        {
+            this.name=name;
+            this.money = money;
+            this.position = position;
+        }
+        
+        public void Improved() 
+        {
+            if (position > 1)
+            {
+                
+                money += 10;
+                if (Promote != null) { Promote?.Invoke($"{name} get {position} position and 10 money's"); }
+                if (position == 1) Console.WriteLine($"{name} get max position ");
+         
+            }
+        }
+        public void Reduced()
+        {
+            if (money > 0)
+            {
+                position -= 1;
+                money -= 5;
+                if (Punishment != null)
+                {
+                    Punishment?.Invoke($"{name} was fine on 5 and have {position} position");
+                    if (money <= 0) { money = 0; Console.WriteLine($"{name} end up money now!"); }
+                }
+                Console.WriteLine($"{name} now have {money} money's");
+            }
+            else Console.WriteLine($"{name} no money's");
+        }
+    }
+}
---
+++ lab9/lab9/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab9
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("exercise1");
+            Director student = new Director("Kirill", 10, 2);
+            student.Promote += message => Console.WriteLine(message);
+            Director student2 = new Director("Petrov", 20, 3);
+            student2.Promote += message => Console.WriteLine(message);
+            student2.Punishment += message => Console.WriteLine(message);
+            Director student3 = new Director("Vasiliy",30,4);
+            student3.Promote += message => Console.WriteLine(message);
+            student3.Punishment += message => Console.WriteLine(message);
+
+            student.Improved();
+            student.Reduced();
+            student2.Improved();
+            student2.Reduced();
+            student3.Improved();
+            student3.Reduced();
+
+            Console.WriteLine("exercise2");
+            string str = "hello my name booba!";
+            Action<string> meth;
+            meth = Application;
+            meth += DeleteLetter;
+            meth += AddLetterAfterLetter;
+            meth += DeleteSpaces;
+            meth += AddPointsAfterSpace;
+            meth(str);
+            Console.WriteLine(str);
+
+        }
+        public static void Application(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '!')
+                    str.Remove(i, 1);
+          
+            }
+        }
+        public static void DeleteSpaces(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                    str.Remove(i, 1);
+            }
+
+        }
+        public static void DeleteLetter(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'h')
+                    str.Remove(i, 1);
+            }
+        }
+        public static void AddLetterAfterLetter(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'y')
+                    str.Insert(i + 1, "S");
+            }
+        }
+        public static void AddPointsAfterSpace(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                    str.Insert(i + 1, ".");
+            }
+        }
+
+    }
+   
+            
+        
+    
+}
